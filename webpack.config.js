6f3bf340f7287745815107a0705b002865427bd4
Dylan Feldman <dfeldman24@gmail.com>
2021-08-27 20:31:33 -0700

    gitignore and webpack set up

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+
+module.exports = {
+  mode: 'production',
+  entry: './frontend/index.html',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'build'),
+    publicPath: '/build/'
+  },
+  module: {
+    rules: [{
+      //loader: 'babel-loader',
+      test: /\.jsx?/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+    }
+  },
+    {
+      test: /\.s[ac]ss$/i,
+      use: ['style-loader', 'css-loader', 'sass-loader']
+    }
+  ]
+  },
+  devServer: {
+		hot: true,
+		port: 8080,
+    static: {
+      directory: path.join(__dirname, '/'),
+      publicPath: '/',
+    },
+		proxy: {
+			'/api/**': {
+        target: 'http://localhost:3000/'
+		  },
+	  }
+  },
+}
