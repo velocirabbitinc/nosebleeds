02c5a4ba94983213ba265e6070e1a54e7f9e0be3
Dylan Feldman <dfeldman24@gmail.com>
2021-08-27 20:32:19 -0700

    basic file structure and templates set up first commit

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'))
+});
+
+
+
+
+
+
+
+app.listen(PORT);
