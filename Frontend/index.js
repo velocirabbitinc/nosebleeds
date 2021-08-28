9d679351e223e4f468de8fef9f298a7e3594ffb7
root <root@DESKTOP-Q45VTQL.localdomain>
2021-08-28 11:38:30 -0700

    testing branch

--- Frontend/index.js
+++ Frontend/index.js
@@ -3,5 +3,7 @@ import { render } from 'react-dom';
 import App from './App.jsx';
 import styles from './styles/styles.scss';
 
+// FRONTEND TEST PUSH
+
 render(<App />, document.getElementById('root'));
 //registerServiceWorker();
