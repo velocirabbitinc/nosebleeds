d33784eb174de4b57c11699d13c105c3d37e6856
Lina Shin <linashin@TLEs-MacBook-Pro.local>
2021-08-31 20:12:14 -0700

    updated action.js

--- client/constants/actionTypes.js
+++ client/constants/actionTypes.js
@@ -1,13 +1,13 @@
- export const ANSWERED_CORRECTLY = 'ANSWERED_CORRECTLY';
- export const ANSWERED_INCORRECTLY = 'ANSWERED_INCORRECTLY';
- export const ADD_CREATED_USER_CARD = 'ADD_CREATED_USER_CARD';
- export const ADD_FLASH_CARD_LIST = 'ADD_FLASH_CARD_LIST ';
- export const ADD_TO_TOPICS_LIST = 'ADD_TO_TOPICS_LIST ';
- export const REMOVE_ONE_TOPICS_LIST = 'REMOVE_ONE_TOPICS_LIST ';
- export const REVEAL_ANSWER = 'REVEAL_ANSWER ';
- export const LOGIN = 'LOGIN';
- export const SIGN_UP = 'SIGN_UP';
- export const CREATE_CARD = 'CREATE-CARD';
- export const RESET_SESSION = 'RESET_SESSION ';
- 
- 
+//Async action types
+export const LOGIN = 'LOGIN'; 
+export const SIGN_UP_USER = 'SIGN_UP_USER'; 
+export const LOG_OUT = 'LOG_OUT'; 
+export const ADD_FAV = 'ADD_FAV'; 
+export const BUY_TIX = 'BUY_TIX'; 
+
+//Sync action types
+export const SIGN_UP_BUTTON = 'SIGN_UP_BUTTON';
+export const CREATE_CARD = 'CREATE_CARD';
+export const HANDLE_CHANGE = 'HANDLE_CHANGE';
+export const SHOW_FAVS = 'SHOW_FAVS';
+export const RETURN_HOME = 'RETURN_HOME';
---
+++ client/constants/constants.js
@@ -0,0 +1,4 @@
+export const LOGIN = 'LOGIN';
+export const SIGN_UP = 'SIGN_UP';
+export const HOMEPAGE_FAVS = 'HOMEPAGE_FAVS';
+export const HOMEPAGE_EVENTS = 'HOMEPAGE_EVENTS';
